b0e1e1ca5c0af78805af761f880adb55796df73b
dronperminov <perminov@ispras.ru>
2023-10-20 09:16:55 +0300

    add profile and index pages

--- src/api/api.py
+++ src/api/api.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from fastapi import APIRouter, Depends
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
 
 from src.api import templates
 from src.utils.auth import get_current_user
@@ -13,4 +13,14 @@ router = APIRouter()
 def index(user: Optional[dict] = Depends(get_current_user)) -> HTMLResponse:
     template = templates.get_template("index.html")
     content = template.render(user=user, page="index")
+    return HTMLResponse(content=content)
+
+
+@router.get("/profile")
+def profile(user: Optional[dict] = Depends(get_current_user)) -> Response:
+    if not user:
+        return RedirectResponse(url="/login")
+
+    template = templates.get_template("profile.html")
+    content = template.render(user=user, page="profile")
     return HTMLResponse(content=content)
