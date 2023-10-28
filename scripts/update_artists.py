9c5c2248bb85cd947876b6f23a94138cbfa83639
dronperminov <perminov@ispras.ru>
2023-10-28 20:16:45 +0300

    fix bug in artists update script

--- scripts/update_artists.py
+++ scripts/update_artists.py
@@ -8,10 +8,10 @@ from src.utils.audio import parse_artist_genres
 
 def main():
     database.connect()
-    artists = database.artists.find({"$or": [
+    artists = list(database.artists.find({"$or": [
         {"genres.0": {"$exists": False}},
         {"creation.0": {"$exists": False}}
-    ]}, {"id": 1, "genres": 1, "creation": 1})
+    ]}))
 
     artist_ids = [artist["id"] for artist in artists]
     artist_genres = parse_artist_genres(artist_ids, random.choice(tokens))
@@ -20,12 +20,12 @@ def main():
     for artist in artists:
         artist_id = artist["id"]
 
-        if not artist.get("genres", []):
+        if not artist.get("genres", []) and artist_genres[artist_id]:
             genres = artist_genres[artist_id]
             database.artists.update_one({"id": artist_id}, {"$set": {"genres": genres}}, upsert=True)
             print(f'Update artists {artist["name"]} genres with {genres}')  # noqa
 
-        if not artist.get("creation", []):
+        if not artist.get("creation", []) and artist_creation[artist_id]:
             creation = list(artist_creation[artist_id])
             database.artists.update_one({"id": artist_id}, {"$set": {"creation": creation}}, upsert=True)
             print(f'Update artists {artist["name"]} creation with {creation}')  # noqa
